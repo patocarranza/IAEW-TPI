b8b66f83e4588285e4f05089502544c6630f0ce4
patocarranza <patocarranza@gmail.com>
2016-06-28 10:38:39 -0300

    firmas de aux endpoint terminadas

--- src/main/java/edu/utn/frc/laew/rest/AuxDataEndpoint.java
+++ src/main/java/edu/utn/frc/laew/rest/AuxDataEndpoint.java
@@ -6,31 +6,49 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import org.datacontract.schemas._2004._07.wcfreservavehiculos_business.VehiculoModel;
 
 @Path("/data")
 public class AuxDataEndpoint {    
 
     @GET
     @Path("/vendedores")
+    @Produces(MediaType.APPLICATION_JSON + ";charset=UTF-8")
     public List<Vendedor> listarVendedores() {
         
     }
     
     @GET
+    @Path("/lugares")
+    @Produces(MediaType.APPLICATION_JSON + ";charset=UTF-8")
+    public List<String> getLugares() {
+        
+    } 
+    
+    @GET
     @Path("/paises")
+    @Produces(MediaType.APPLICATION_JSON + ";charset=UTF-8")
     public List<Pais> listarPaises() {
         
     }
     
     @GET
     @Path("/ciudades/{idPais}")
-    public List<Pais> listarPaises( @PathParam("idPais") Integer idPais) {
+    @Produces(MediaType.APPLICATION_JSON + ";charset=UTF-8")
+    public List<Pais> listarCiudades( @PathParam("idPais") Integer idPais) {
         
     }
     
     @GET
-    @Path("/vehiculos/{idCiudad}")
-    public List<Pais> listarVehiculos( @PathParam("idCiudad") Integer idCiudad) {
+    @Path("/vehiculos")
+    @Produces(MediaType.APPLICATION_JSON + ";charset=UTF-8")
+    public List<VehiculoModel> listarVehiculos( @QueryParam("idCiudad") Integer idCiudad,
+                                                @QueryParam("fecRet") String fechaRetiro,
+                                                @QueryParam("fecDev") String fechaDevolucion) {
         
-    }         
+    }
+        
 }
