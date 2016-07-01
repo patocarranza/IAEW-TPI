b779b704fbb1a6c3f8fe2507d065e5fa0368e081
Angelo <angelowolf21@gail.com>
2016-07-01 15:32:37 -0300

    merge

---
+++ target/tpi-iaew/WEB-INF/classes/import.sql
@@ -0,0 +1,7 @@
+
+
+insert into Vendedor(id,nombre) values(1, "Pablo Gimenez");
+insert into Vendedor(id,nombre) values(2, "Andres Gonzalez");
+insert into Vendedor(id,nombre) values(3, "Nicolas Gomez");
+insert into Vendedor(id,nombre) values(4, "Pedro Alvarez");
+insert into Vendedor(id,nombre) values(5, "Angelo Wolf");
